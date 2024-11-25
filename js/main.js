8a1803d56bb5ef46f73c7a021c3630cb980d2afc
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-25 14:28:41 -0500

    add emojis and fix bugs

--- js/main.js
+++ js/main.js
@@ -1,60 +1,42 @@
+const emojis = ['🥳', '🔥', '🌈', '✨', '💪', '🎉', '☀️'];
 
-onload = () => {
+onload =  () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const name = urlParams.get('name') || 'Mari'; // Si no hay nombre, usa "Mari" como valor predeterminado
+  const name = urlParams.get('name') || 'Usuario Desconocido 😊'; // Si no hay nombre, usa "Mari" como valor predeterminado
 
   const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
+    document.body.classList.remove('not-loaded');
 
     // Obtener el día usando Intl.DateTimeFormat
-    const diaSemana = new Intl.DateTimeFormat('es', { weekday: 'long' }).format(new Date());
-
+    const diaSemana = new Intl.DateTimeFormat('es', { weekday: 'long' }).format(
+      new Date()
+    );
+    const emojiDelDia =
+      emojis[Math.floor(Math.random() * emojis.length)] || '😊';
+    const selectedEmoji =
+      emojis[Math.floor(Math.random() * emojis.length)] || '😊';
     // Capitalizar primera letra y concatenar con "Feliz"
-    const diaCapitalizado = diaSemana.charAt(0).toUpperCase() + diaSemana.slice(1);
-    const titles = (`Feliz ${diaCapitalizado} ${name}`).split('');
+    const diaCapitalizado =
+      diaSemana.charAt(0).toUpperCase() + diaSemana.slice(1);
+    const titles =
+      `${selectedEmoji}Feliz ${diaCapitalizado} ${name}!!${emojiDelDia}`.split(
+        ''
+      );
     const titleElement = document.getElementById('title');
     let index = 0;
 
     function appendTitle() {
       if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
+        titleElement.textContent += titles[index];
         index++;
-        setTimeout(appendTitle, 300);
+        setTimeout(appendTitle, 200);
       }
     }
 
     appendTitle();
     clearTimeout(c);
-  }, 1000);
+  }, 500);
 };
-
-
-/*onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    
-    // Obtener el día usando Intl.DateTimeFormat
-    const diaSemana = new Intl.DateTimeFormat('es', { weekday: 'long' })
-                         .format(new Date());
-    
-    // Capitalizar primera letra y concatenar con "Feliz"
-    const diaCapitalizado = diaSemana.charAt(0).toUpperCase() + diaSemana.slice(1);
-    const titles = (`Feliz ${diaCapitalizado} Mari`).split('');
-    const titleElement = document.getElementById('title');
-    let index = 0;
-
-    function appendTitle() {
-      if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
-        index++;
-        setTimeout(appendTitle, 300);
-      }
-    }
-
-    appendTitle();
-    clearTimeout(c);
-  }, 1000);
-};*/
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
@@ -69,26 +51,21 @@ function getRandomColor() {
 function changeGradient() {
   const flowerLines = document.querySelectorAll('.flower__line');
   const leaf = document.querySelectorAll('.flower__line__leaf ');
-  
+
   const opacity1 = Math.random().toFixed(2);
   const opacity2 = Math.random().toFixed(2);
   const color1 = getRandomColor();
   const color2 = getRandomColor();
-  
-  flowerLines.forEach(line => {
+
+  flowerLines.forEach((line) => {
     const newGradient = `linear-gradient(to left, rgba(0, 0, 0, ${opacity1}), transparent, rgba(255, 255, 255, ${opacity2})), linear-gradient(to top, transparent 10%, ${color1}, ${color2})`;
     line.style.backgroundImage = newGradient;
   });
 
-  leaf.forEach(line => {
+  leaf.forEach((line) => {
     const newGradient = `linear-gradient(to top, rgba(255, 255, 255, ${opacity2}), ${color2})`;
     line.style.backgroundImage = newGradient;
   });
-
-
-
-
-
 }
 
 // Función para cambiar el color de las hojas
@@ -96,8 +73,8 @@ function changeLeaves() {
   const leaves = document.querySelectorAll('.flower__leaf');
   const baseColor = getRandomColor();
   const highlightColor = getRandomColor();
-  
-  leaves.forEach(leaf => {
+
+  leaves.forEach((leaf) => {
     leaf.style.backgroundColor = baseColor;
     leaf.style.backgroundImage = `linear-gradient(to top, ${baseColor}, ${highlightColor})`;
     // Mantener el efecto de brillo
@@ -111,7 +88,7 @@ function changeBackground() {
   const color1 = getRandomColor();
   const color2 = getRandomColor();
   const color3 = getRandomColor();
-  
+
   const newGradient = `
     radial-gradient(ellipse at top, transparent 0%, var(--dark-color)),
     radial-gradient(ellipse at bottom, var(--dark-color), ${color1}),
@@ -120,11 +97,15 @@ function changeBackground() {
     repeating-linear-gradient(148deg, ${color2} 0px, ${color2} 19px, transparent 19px, transparent 22px),
     linear-gradient(90deg, ${color3}, ${getRandomColor()})
   `;
-  
+
   night.style.backgroundImage = newGradient;
 }
 
 // Agregar event listeners a los botones
-document.getElementById('changeGradientBtn').addEventListener('click', changeGradient);
-document.getElementById('changeLeavesBtn').addEventListener('click', changeLeaves);
-document.getElementById('changeBackgroundBtn').addEventListener('click', changeBackground);
+document
+  .getElementById('changeGradientBtn')
+  .addEventListener('click', changeGradient);
+document
+  .getElementById('changeLeavesBtn')
+  .addEventListener('click', changeLeaves);
+//document.getElementById('changeBackgroundBtn').addEventListener('click', changeBackground);
